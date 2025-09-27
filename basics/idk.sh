7fda382aa59d1d4d8e7e101b6e8c0f8b17cdbfad
NoJamesHere <davidedwin2007@gmail.com>
2025-09-27 19:51:59 +0200

    testing out something

--- basics/idk.sh
+++ basics/idk.sh
@@ -1,8 +1,10 @@
 # experimenting with those things
 echo "You are currently in $(pwd)"
 echo {1..5} "<-- echo {1..5}"
+# Input.. What does -p even mean?
 read -p "First number: " a
 read -p "Second number: " b
+
 
 echo
 echo {${a}..${b}}
