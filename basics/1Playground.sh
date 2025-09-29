32f50211ce9b3165e9463c9f6787c880ed8976a4
NoJamesHere <davidedwin2007@gmail.com>
2025-09-29 18:53:05 +0200

    More styling i guess

--- basics/1Playground.sh
+++ basics/1Playground.sh
@@ -38,13 +38,20 @@ else
   string="$a $b"
 fi
 
+for i in {1..4}; do echo; done
+
 echo -e "${BLUE}^ seq $string ^${RESET}"
 
 # -- What is the difference between $VAL, ${VAL}, $(VAL), $((VAL))? --
 
+echo; echo -e "${HEADER} How to use \$VAL.${RESET}"  # --- $VAL ---
+                                                     #
+name=Anonymous                                       # $V : This is a very basic concept. E.g. (tl;1) name and
+echo; echo "Hello $name."                            # $V : Call it with `$name`. Easy.
+
 echo; echo -e "${HEADER} How to use \${}.${RESET}"   # --- ${} ---
                                                      #
-name=Anonymous                                       # ${} : For starters, it's needed for color ansi escape codes. otherwise it literally just prints `\033[whatever;1m`.
+                                                     # ${} : For starters, it's needed for color ansi escape codes. otherwise it literally just prints `\033[whatever;1m`.
 echo; echo -e "${YELLOW} ${name}_2025 ${RESET}"      # ${} : Also used for something like this (tl;6). Otherwise, if it were $name_2025, bash would literally think we meant
                                                      # ${} : a variable called name_2025, but we wanted to append "_2025" to the variable $name. Makes sense, right?
 echo; echo "${name^^}, ${name,,}, ${name:5:8}"       # ${} : It also comes in handy when we need to do string operations, e.g. uppercasing it.
@@ -64,7 +71,7 @@ numB=2                                                       #
                                                              #
 echo; echo "A:${numA} ; B:${numB}"                           # $(() : This is used to perform arithmetic calculations.
 echo; echo "A+B: $((numA + numB))"                           # $(() : Here (tl;18) it calculates the sum of A and B.
-((numB++))                                                   # $(() : On (tl;1) it increments B by 1, causing it to be
-echo; echo "B++ ; B:${numB}"                                 # $(() : 3 now instead of the previous 2. (easy, right?)
+((numB++))                                                   # $(() : On (tl;1) it increments B by 1, causing it now to
+echo; echo "B++ ; B:${numB}"                                 # $(() : be 3 instead of the previous 2. (easy, right?)
 
 exit 0
